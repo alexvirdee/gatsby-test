01e7d2525daaa48d2bc0858eb25c1d6e6cb596bb
Alexinder Virdee <alex.virdee08@gmail.com>
2020-06-09 13:30:51 -0400

    Edit configuration file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
+    title: `Welcome to my blog`,
+    author: `Alex Virdee`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://hungry-bassi-27b875.netlify.app/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `alex_virdee`,
     },
   },
   plugins: [
