074973b910be50a023c37d69e5d07aad1dc239dc
Alexinder Virdee <alex.virdee08@gmail.com>
2020-06-15 15:33:23 -0400

    Remove lazy load image component for index. App issue with hooks working to resolve

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -29,7 +28,7 @@ class IndexPage extends React.Component {
           Resume
         </a> */}
       </div>
-        <LazyLoadImage style={{ margin: 0 }} src="./423.jpg" alt="Web Developer" />
+        <img style={{ margin: 0 }} src="./423.jpg" alt="Web Developer" />
         <h1>
           Hello World{" "}
           <span role="img" aria-label="wave emoji">
