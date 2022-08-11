c4fe484e5a44332d1cedeef362985cf3bb09560c
Alexinder Virdee <alex.virdee08@gmail.com>
2022-08-11 12:47:06 -0400

    Inner components for searchPosts

--- src/components/searchPosts.js
+++ src/components/searchPosts.js
@@ -6,7 +6,7 @@ import * as queryString from "query-string";
 
 import { rhythym } from "../utils/typography";
 
-const searchPosts = ({ results }) => {
+const SearchedPosts = ({ results }) => {
     results.length > 0 ? (
         results.map(node => {
             const date = node.date
@@ -24,6 +24,12 @@ const searchPosts = ({ results }) => {
                             {title}
                         </Link>
                     </h3>
+                    <small>{date}</small>
+                    <p
+                     dangerouslySetInnerHTML={{
+                        __html: description || excerpt,
+                     }}
+                    />
                 </div>
             )
         })
@@ -33,6 +39,72 @@ const searchPosts = ({ results }) => {
         }}>
             Sorry! No posts match this search.
         </p>
+    )
+}
+
+const AllPosts = ({ posts }) => (
+    <div style={{
+        margin: "20px 0 40px"
+    }}>
+        {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug 
+
+            return (
+                <div key={node.fields.slug}>
+                    <h3 style={{
+                        marginBottom: rhythm(1 / 4)
+                    }}>
+                        <Link style={{ boxShadow: `none` }} to={`/blog/${node.fields.slug}`}>
+                            {title}
+                        </Link>
+                    </h3>
+                    <small>{node.frontmatter.date}</small>
+                    <p
+                    dangerouslySetInnerHTML={{
+                        __html: node.frontmatter.description || node.excerpt,
+                        }}
+                    />
+                </div>
+            )
+        })}
+    </div>
+)
+
+const SearchPosts = ({ posts, localSearchBlog, location, navigate }) => {
+    const { search } = queryString.parse(location.search)
+    const [query, setQuery] = useState(search || "")
+
+    const results = useFlexSearch(
+        query,
+        localSearchBlog.index,
+        JSON.parse(localSearchBlog.store)
+    )
+
+    return (
+        <>
+         <SearchBar>
+         <svg
+          focusable="false"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+        </svg>
+        <input
+            id="search"
+            type="search"
+            placeholder="Search all posts"
+            value={query}
+            onChange={e => {
+                navigate(
+                    e.target.value ? `/blog/?search=${e.target.value}` : "/blog/"
+                )
+                setQuery(e.target.value)
+            }}
+        />
+        </SearchBar>
+        {query ? <SearchedPosts results={results} /> : <AllPosts post={posts} /> }
+        </>
     )
 }
 
@@ -69,4 +141,4 @@ const SearchBar = styled.div`
   }
 `
 
-export default searchPosts
+export default SearchPosts;
